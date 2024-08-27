c385dac7e1aca6a4e80654ffc3c9345eb940fe7d
play10grounds <play10grounds@hotmail.com>
2024-08-27 11:12:13 +0900

    gallery list 관련 코드 추가

--- app/service/gallery.py
+++ app/service/gallery.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 
 from fastapi import Form
-from sqlalchemy import insert
+from sqlalchemy import insert, select
 from sqlalchemy.exc import SQLAlchemyError
 
 from app.model.gallery import Gallery, GalAttach
@@ -57,7 +57,20 @@ class GalleryService:
             db.rollback()
 
 
+    @staticmethod
+    def select_gallery(cpg, db):
+        try:
+            stmt = select(Gallery.gno, Gallery.title, Gallery.userid,
+                Gallery.regdate, Gallery.views, GalAttach.fname)\
+                .join_from(Gallery, GalAttach)\
+                .order_by(Gallery.gno.desc()).limit(25)
+            result = db.execute(stmt)
 
+            return result
+
+        except SQLAlchemyError as ex:
+            print(f'▶▶▶ select_gallery에서 오류발생 : {str(ex)} ')
+            db.rollback()
 
 
 
