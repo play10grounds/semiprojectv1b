eed99bb7e313c07b86e01b854d26bb604742ebb9
play10grounds <play10grounds@hotmail.com>
2024-09-03 11:26:55 +0900

    sqlite모델을 mariadb로 마이그레이션 수정

--- app/model/gallery.py
+++ app/model/gallery.py
@@ -11,7 +11,7 @@ class Gallery(Base):
 
     gno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     title: Mapped[str] = mapped_column(String(100), index=True)
-    userid: Mapped[str] = mapped_column(String(18), ForeignKey(String(250),'member.userid'), index=True)
+    userid: Mapped[str] = mapped_column(String(18), ForeignKey('member.userid'), index=True)
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     views: Mapped[int] = mapped_column(default=0)
     contents: Mapped[str] = mapped_column(Text)
