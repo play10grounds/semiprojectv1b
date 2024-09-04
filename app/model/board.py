a05c08bbbb797d1cced07de52f0fddc4f08876ae
play10grounds <play10grounds@hotmail.com>
2024-09-04 16:09:15 +0900

    cascade 설정 추가 default 제약 추가

--- app/model/board.py
+++ app/model/board.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import ForeignKey, String, Text, Integer
+from sqlalchemy import ForeignKey, String, Text, Integer, func, Column, text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.model.base import Base
@@ -10,13 +10,13 @@ class Board(Base):
     __tablename__ = 'board'
     __table_args__ = {'sqlite_autoincrement': True}
 
-    bno: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True, index=True)
+    bno: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     title: Mapped[str] = mapped_column(String(100), index=True)
     userid: Mapped[str] = mapped_column(String(18),ForeignKey('member.userid'), index=True)
-    regdate: Mapped[datetime] = mapped_column(default=datetime.now)
-    views: Mapped[int] = mapped_column(default=0)
+    regdate: Mapped[datetime] = mapped_column(default=datetime.now, server_default=func.now())
+    views: Mapped[int] = Column(Integer, default=0, server_default=text("0"))
     contents: Mapped[str] = mapped_column(Text)
-    replys = relationship('Reply', back_populates='board')
+    replys = relationship('Reply', back_populates='board', cascade='all, delete-orphan')
 
 
 class Reply(Base):
@@ -27,8 +27,9 @@ class Reply(Base):
     reply: Mapped[str] = mapped_column(String(250), index=True)
     userid: Mapped[str] = mapped_column(String(18), ForeignKey('member.userid'), index=True)
     # regdate: Mapped[datetime] = mapped_column(default=datetime.now)
-    regdate: Mapped[datetime] = mapped_column(default=lambda: datetime.now().replace(microsecond=0))
-    bno: Mapped[int] = mapped_column(ForeignKey('board.bno'))
-    rpno: Mapped[int] = mapped_column(ForeignKey('reply.rno'))
+    regdate: Mapped[datetime] = mapped_column(default=lambda: datetime.now().replace(microsecond=0), server_default=func.now())
+    bno: Mapped[int] = mapped_column(ForeignKey('board.bno', ondelete='CASCADE'))
+    rpno: Mapped[int] = mapped_column(ForeignKey('reply.rno', ondelete='CASCADE'))
     board = relationship('Board', back_populates='replys')
+
 
--- app/model/pds.py
+++ app/model/pds.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import ForeignKey, String, Text
+from sqlalchemy import ForeignKey, String, Text, Column, Integer, text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.model.base import Base
@@ -14,7 +14,7 @@ class Pds(Base):
     title: Mapped[str] = mapped_column(String(100), index=True)
     userid: Mapped[str] = mapped_column(String(18), ForeignKey('member.userid'), index=True)
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
-    views: Mapped[int] = mapped_column(default=0)
+    views = Column(Integer, default=0, server_default=text("0"))
     contents: Mapped[str] = mapped_column(Text)
     attachs = relationship('PdsAttach', back_populates='pds')  # 하나의 gallery는 하나 이상의 attach 존재 (1:n)
 
@@ -28,12 +28,4 @@ class PdsAttach(Base):
     fsize: Mapped[int] = mapped_column(default=0)
     regdate: Mapped[datetime] = mapped_column(default=datetime.now)
     pds = relationship('Pds', back_populates='attachs')  # 하나의 attach는 하나의 gallery에 속함 (1:1)
-
-
-
-
-
-
-
-
 
